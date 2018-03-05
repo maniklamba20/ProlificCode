746a8f8d95217401f51706d26df161960ba37864
Manik Lamba <maniklamba@maniks-macbook-pro-2.local>
2018-03-05 14:06:09 -0500

    Verify First Commit from Xcode

--- ProlificLib/ViewControllers/ViewController.swift
+++ ProlificLib/ViewControllers/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchData()
+        //Verify Changes
         // Do any additional setup after loading the view, typically from a nib.
     }
 
